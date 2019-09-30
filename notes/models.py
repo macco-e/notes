ef5257fff89af48e8d1b39b759caad008e504fac
Makoto Esaki <esaki6101@gmail.com>
2019-10-01 01:29:52 +0900

    #2 add sign-up form

--- notes/models.py
+++ notes/models.py
@@ -3,24 +3,6 @@ from django.contrib.auth.models import AbstractUser, UserManager
 from django_mysql.models import ListCharField
 
 
-# class AccountManager(UserManager):
-#     def _create_user(self, username, password, **extra_fields):
-#         user = self.model(**extra_fields)
-#         user.set_password(password)
-#         user.save(using=self._db)
-#         return user
-#
-#     def create_user(self, password=None, **extra_fields):
-#         extra_fields.setdefault('is_staff', False)
-#         extra_fields.setdefault('is_superuser', False)
-#         return self._create_user(password, **extra_fields)
-#
-#     def create_superuser(self, username, password, **extra_fields):
-#         extra_fields.setdefault('is_staff', True)
-#         extra_fields.setdefault('is_superuser', True)
-#         return self._create_user(password, **extra_fields)
-
-
 class Account(AbstractUser):
     noted_num = models.IntegerField(default=0)
     noted_tables = ListCharField(
