3bda45ef3d8a9496d7a37fa4e026f8e2f6e758dc
Makoto Esaki <esaki6101@gmail.com>
2019-10-01 02:31:11 +0900

    #4 add /home view template

--- notes/urls.py
+++ notes/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import include, path
 
-from .views import sign_up_view, create_user, login_view, HomeView
+from .views import sign_up_view, create_user, login_view, HomeView, redirect_to
 
 app_name = 'notes'
 urlpatterns = [
@@ -9,4 +9,5 @@ urlpatterns = [
     path('create_user/', create_user, name='create_user'),
     path('login/', login_view, name='login'),
     path('home/', HomeView.as_view(), name='home'),
+    path('redirect_to/', redirect_to, name='redirect_to')
 ]
