614841b7e997077f8b2672ecadcabae5810dd6fc
Makoto Esaki <esaki6101@gmail.com>
2019-10-01 23:57:12 +0900

    #21 Add follow function

--- notes/urls.py
+++ notes/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import include, path
 
-from .views import sign_up_view, create_user, login_view, HomeView, redirect_to, logout_view, UserDetailView, PostNoteView, SettingsView
+from .views import sign_up_view, create_user, login_view, HomeView, redirect_to, logout_view, UserDetailView, PostNoteView, SettingsView, follow, unfollow
 
 app_name = 'notes'
 urlpatterns = [
@@ -14,4 +14,6 @@ urlpatterns = [
     path('redirect_to/', redirect_to, name='redirect_to'),
     path('create/', PostNoteView.as_view(), name='post_note'),
     path('settings/<int:pk>', SettingsView.as_view(), name='settings'),
+    path('follow/<int:pk>', follow, name='follow'),
+    path('unfollow/<int:pk>', unfollow, name='unfollow'),
 ]
