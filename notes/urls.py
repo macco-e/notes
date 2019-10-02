a45c6428dd794ff4152a9f215de216541a2afa4c
Makoto Esaki <esaki6101@gmail.com>
2019-10-03 03:40:26 +0900

    #43 Fix timeline order bug

--- notes/urls.py
+++ notes/urls.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 from django.urls import include, path
 
 from .views import sign_up_view, create_user, login_view, HomeView, logout_view, UserDetailView, PostNoteView, SettingsView, follow, unfollow, UserFollowListView, UserFollowerListView, UsersListView
-from .views import UsersSearchView, search_redirect, HomeNotesSearchView, search_home_redirect, UserNoteSearchView, search_user_redirect
+from .views import UsersSearchView, search_redirect, HomeNotesSearchView, search_home_redirect, UserNoteSearchView, search_user_redirect, SearchListView
+from .views import search_notes_redirect, SearchedListView
 
 app_name = 'notes'
 urlpatterns = [
@@ -14,6 +15,10 @@ urlpatterns = [
     path('home/', HomeView.as_view(), name='home'),
     path('search_home_redirect/', search_home_redirect, name='search_home_redirect'),
     path('home/search/<str:search_word>', HomeNotesSearchView.as_view(), name='home_notes_search'),
+
+    path('search/', SearchListView.as_view(), name="search"),
+    path('search_notes_redirect', search_notes_redirect, name="search_notes_redirect"),
+    path('search/<str:search_word>', SearchedListView.as_view(), name="searched"),
 
     path('user/<int:pk>', UserDetailView.as_view(), name='detail'),
     path('user/<int:pk>/follow', UserFollowListView.as_view(), name='follow_list'),
