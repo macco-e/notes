56706e082ba0613d249bde75e8fe94ebe480e202
Makoto Esaki <esaki6101@gmail.com>
2019-10-14 02:15:17 +0900

    fix

--- config/settings/local.py
+++ config/settings/local.py
@@ -36,7 +36,7 @@ DATABASES = {
         'USER': 'root',
         'PASSWORD': 'root',
         # 'HOST': 'db',
-        'HOST': '0.0.0.0',
+        'HOST': '127.0.0.1',
         'PORT': 3306,
         'OPTIONS': {
             'charset': 'utf8mb4',
