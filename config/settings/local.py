2b88e4e5a445fb8c4eb1cb78d1094db108650be3
Makoto Esaki <esaki6101@gmail.com>
2019-10-14 14:52:13 +0900

    fix circleci settings

--- config/settings/local.py
+++ config/settings/local.py
@@ -35,7 +35,8 @@ DATABASES = {
         'NAME': 'notes_project',
         'USER': 'root',
         'PASSWORD': 'root',
-        'HOST': 'db',
+        # 'HOST': 'db',
+        'HOST': '127.0.0.1',
         'PORT': 3306,
         'OPTIONS': {
             'charset': 'utf8mb4',
